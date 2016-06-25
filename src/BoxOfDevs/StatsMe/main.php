f5bcf9e8a2a31b4c02384732c53ad6a6726c4818
HimbeersaftLP <elias.nestl@outlook.de>
2016-06-25 10:14:45 +0200

    Some fixes
    - Now gets messages for "Player not found" and "No Permission" from the config
    - Now it's testing if the player exists when doing "/stats [player]"

--- src/BoxOfDevs/StatsMe/main.php
+++ src/BoxOfDevs/StatsMe/main.php
@@ -51,32 +51,37 @@ class Main extends PluginBase implements Listener{
                          break;
                     }elseif(isset($args[0]) && $sender->hasPermission("statsme.other")){
                          $player = $this->getServer()->getPlayer($args[0]);
-                         $stats = $this->config->get("stats");
-                         $stats = str_replace("{firstline}", $this->config->get("firstline_otherstats"), $stats);
-                         $pname = $player->getName();
-                         $stats = str_replace("{name}", $pname, $stats);
-                         $x = round($player->x, 0);
-                         $y = round($player->y, 0);
-                         $z = round($player->z, 0);
-                         $stats = str_replace("{xyz}", $x.", ".$y.", ".$z, $stats);
-                         $health = $player->getHealth();
-                         $stats = str_replace("{health}", $health, $stats);
-                         $maxhealth = $player->getMaxHealth();
-                         $stats = str_replace("{maxhealth}", $maxhealth, $stats);
-                         $heartsymbol = $this->config->get("heartsymbol");
-                         $hearts = str_repeat($heartsymbol, $health/2);
-                         $nohearts = str_repeat($heartsymbol, $maxhealth/2 - $health/2);
-                         $stats = str_replace("{hearts}", "§c".$hearts."§f".$nohearts, $stats);
-                         $stats = str_replace("{coins}", $this->economyapi->mymoney($player->getName()), $stats);
-                         $stats = str_replace("{line}", "\n", $stats);
-                         $stats = str_replace("&", "§", $stats);
-                         $sender->sendMessage($stats);
+                         if($player instanceof Player){
+                              $stats = $this->config->get("stats");
+                              $stats = str_replace("{firstline}", $this->config->get("firstline_otherstats"), $stats);
+                              $pname = $player->getName();
+                              $stats = str_replace("{name}", $pname, $stats);
+                              $x = round($player->x, 0);
+                              $y = round($player->y, 0);
+                              $z = round($player->z, 0);
+                              $stats = str_replace("{xyz}", $x.", ".$y.", ".$z, $stats);
+                              $health = $player->getHealth();
+                              $stats = str_replace("{health}", $health, $stats);
+                              $maxhealth = $player->getMaxHealth();
+                              $stats = str_replace("{maxhealth}", $maxhealth, $stats);
+                              $heartsymbol = $this->config->get("heartsymbol");
+                              $hearts = str_repeat($heartsymbol, $health/2);
+                              $nohearts = str_repeat($heartsymbol, $maxhealth/2 - $health/2);
+                              $stats = str_replace("{hearts}", "§c".$hearts."§f".$nohearts, $stats);
+                              $stats = str_replace("{coins}", $this->economyapi->mymoney($player->getName()), $stats);
+                              $stats = str_replace("{line}", "\n", $stats);
+                              $stats = str_replace("&", "§", $stats);
+                              $sender->sendMessage($stats);
+                              break;
+                         }else{
+                              $sender->sendMessage($this->config->get("player_notfound_msg"));
+                         }
                          break;
                     }elseif(!$sender instanceof Player){
                          $sender->sendMessage("Please use this command ingame!");
                          break;
                     }else{
-                         $sender->sendMessage("You don't have the permission to perform this command.");
+                         $sender->sendMessage($this->config->get("noperm_msg"));
                          break;
                     }
           }

53a1268a5fd90a6ad701527ad9922419f993fd92
HimbeersaftLP <elias.nestl@outlook.de>
2016-07-04 17:28:50 +0200

    Added WinAPI support + StatsMe API
    Use getStats to get the StatsMe message of the given player

--- src/BoxOfDevs/StatsMe/main.php
+++ src/BoxOfDevs/StatsMe/main.php
@@ -8,7 +8,10 @@ use pocketmine\utils\Config;
 use pocketmine\Server;
 use pocketmine\Player;
 use pocketmine\permission\Permission;
+
 use onebone\economyapi\EconomyAPI;
+use BoxOfDevs\WinAPI\main;
+
 class Main extends PluginBase implements Listener{
      
      public function onEnable(){
@@ -23,59 +26,58 @@ class Main extends PluginBase implements Listener{
                $this->economyapi = null;
                $this->getLogger()->notice("EconomyAPI support in StatsMe disabled!");
           }
+          if($this->getServer()->getPluginManager()->getPlugin("WinAPI") != null){
+               $this->winapi = $this->getServer()->getPluginManager()->getPlugin("WinAPI");
+               $this->getLogger()->notice("WinAPI support in StatsMe enabled!");
+          }else{
+               $this->winapi = null;
+               $this->getLogger()->notice("WinAPI support in StatsMe disabled!");
+          }
      }
      
+     public function getStats($player, $ptype = "me"){
+          $stats = $this->config->get("stats");
+          if($ptype === "me"){
+               $stats = str_replace("{firstline}", $this->config->get("firstline_mystats"), $stats);
+          }else{
+               $stats = str_replace("{firstline}", $this->config->get("firstline_otherstats"), $stats);
+          }
+          $pname = $player->getName();
+          $stats = str_replace("{name}", $pname, $stats);
+          $x = round($player->x, 0);
+          $y = round($player->y, 0);
+          $z = round($player->z, 0);
+          $stats = str_replace("{xyz}", $x.", ".$y.", ".$z, $stats);
+          $health = $player->getHealth();
+          $stats = str_replace("{health}", $health, $stats);
+          $maxhealth = $player->getMaxHealth();
+          $stats = str_replace("{maxhealth}", $maxhealth, $stats);
+          $heartsymbol = $this->config->get("heartsymbol");
+          $hearts = str_repeat($heartsymbol, $health/2);
+          $nohearts = str_repeat($heartsymbol, $maxhealth/2 - $health/2);
+          $stats = str_replace("{hearts}", "§c".$hearts."§f".$nohearts, $stats);
+          if($this->economyapi != null){
+               $stats = str_replace("{coins}", $this->economyapi->mymoney($player->getName()), $stats);
+          }
+          if($this->winapi != null){
+               $stats = str_replace("{wins}", $this->winapi->getwins($player->getName()), $stats);
+          }
+          $stats = str_replace("{line}", "\n", $stats);
+          $stats = str_replace("&", "§", $stats);
+          return $stats;
+     }
+
      public function onCommand(CommandSender $sender, Command $command, $label, array $args){
           switch($command->getName()){
                case "stats":
                     if($sender instanceof Player && !isset($args[0]) && $sender->hasPermission("statsme.my")){
-                         $stats = $this->config->get("stats");
-                         $stats = str_replace("{firstline}", $this->config->get("firstline_mystats"), $stats);
-                         $sname = $sender->getName();
-                         $stats = str_replace("{name}", $sname, $stats);
-                         $x = round($sender->x, 0);
-                         $y = round($sender->y, 0);
-                         $z = round($sender->z, 0);
-                         $stats = str_replace("{xyz}", $x.", ".$y.", ".$z, $stats);
-                         $health = $sender->getHealth();
-                         $stats = str_replace("{health}", $health, $stats);
-                         $maxhealth = $sender->getMaxHealth();
-                         $stats = str_replace("{maxhealth}", $maxhealth, $stats);
-                         $heartsymbol = $this->config->get("heartsymbol");
-                         $hearts = str_repeat($heartsymbol, $health/2);
-                         $nohearts = str_repeat($heartsymbol, $maxhealth/2 - $health/2);
-                         $stats = str_replace("{hearts}", "§c".$hearts."§f".$nohearts, $stats);
-                         if($this->economyapi != null){
-                              $stats = str_replace("{coins}", $this->economyapi->mymoney($sender->getName()), $stats);
-                         }
-                         $stats = str_replace("{line}", "\n", $stats);
-                         $stats = str_replace("&", "§", $stats);
+                         $stats = $this->getStats($sender);
                          $sender->sendMessage($stats);
                          break;
                     }elseif(isset($args[0]) && $sender->hasPermission("statsme.other")){
                          $player = $this->getServer()->getPlayer($args[0]);
                          if($player instanceof Player){
-                              $stats = $this->config->get("stats");
-                              $stats = str_replace("{firstline}", $this->config->get("firstline_otherstats"), $stats);
-                              $pname = $player->getName();
-                              $stats = str_replace("{name}", $pname, $stats);
-                              $x = round($player->x, 0);
-                              $y = round($player->y, 0);
-                              $z = round($player->z, 0);
-                              $stats = str_replace("{xyz}", $x.", ".$y.", ".$z, $stats);
-                              $health = $player->getHealth();
-                              $stats = str_replace("{health}", $health, $stats);
-                              $maxhealth = $player->getMaxHealth();
-                              $stats = str_replace("{maxhealth}", $maxhealth, $stats);
-                              $heartsymbol = $this->config->get("heartsymbol");
-                              $hearts = str_repeat($heartsymbol, $health/2);
-                              $nohearts = str_repeat($heartsymbol, $maxhealth/2 - $health/2);
-                              $stats = str_replace("{hearts}", "§c".$hearts."§f".$nohearts, $stats);
-                              if($this->economyapi != null){
-                                   $stats = str_replace("{coins}", $this->economyapi->mymoney($player->getName()), $stats);
-                              }
-                              $stats = str_replace("{line}", "\n", $stats);
-                              $stats = str_replace("&", "§", $stats);
+                              $stats = $this->getStats($player, "other");
                               $sender->sendMessage($stats);
                               break;
                          }else{
